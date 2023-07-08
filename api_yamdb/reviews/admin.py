076dd5ceec9f1228ebe45b9c26edd6e372e36e13
Iskander Alimov <rocketikman@yandex.ru>
2023-07-09 01:53:20 +0300

    fin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,11 +3,7 @@ from django.contrib import admin
 from .models import Category, Genre, Title, User, Review, Comment
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'email', 'username', 'bio', 'role', 'first_name', 'last_name')
-    empty_value_display = '-пусто-'
+admin.site.register(User)
 
 
 @admin.register(Category)
