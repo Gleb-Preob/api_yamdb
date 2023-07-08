237e9abaf9d042de7b9f7813a65c1f7b841cd2ef
Iskander Alimov <rocketikman@yandex.ru>
2023-07-08 22:46:40 +0300

    updated

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -144,18 +144,3 @@ class RegisterDataSerializer(serializers.ModelSerializer):
 class TokenSerializer(serializers.Serializer):
     username = serializers.CharField()
     confirmation_code = serializers.CharField()
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    review = serializers.SlugRelatedField(
-        slug_field='text',
-        read_only=True
-    )
-    author = serializers.SlugRelatedField(
-        slug_field='username',
-        read_only=True
-    )
-
-    class Meta:
-        model = Comment
-        fields = '__all__'
